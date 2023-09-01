8eb8ce6006c7ca5a92eba375a2dd204e146ace9a
aramoto99 <aramoto.masahiro@gmail.com>
2023-09-01 13:34:04 +0900

    Merge changes from the original fork

--- examples/mpi/sphere_cpu_1node_1try/user.py
+++ examples/mpi/sphere_cpu_1node_1try/user.py
@@ -1,6 +1,11 @@
 # from time import sleep
 import numpy as np
+<<<<<<< HEAD:examples/mpi/sphere_cpu_1node_1try/user.py
 from aiaccel.util import aiaccel
+=======
+
+from aiaccel.experimental.mpi.util import aiaccel
+>>>>>>> d4c097c (Merge changes from the original fork):examples/experimental/mpi/sphere_cpu_1node_1try/user.py
 
 
 def main(p):
